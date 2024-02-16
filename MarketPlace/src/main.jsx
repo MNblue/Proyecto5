5a40c0c3cdd4593829270d0b05727aa938224fa4
Lorena-Rodriguez <lorenarodriguezc@gmail.com>
2024-02-16 20:19:10 +0100

    add card form to FormAddProduct

--- MarketPlace/src/main.jsx
+++ MarketPlace/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Admin from './views/admin/Admin';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import User from './views/user/User';
 
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<User />} />
-        {/* <Route path="/admin" element={<Admin />} /> */}
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
---
+++ MarketPlace/src/views/admin/Admin.jsx
@@ -0,0 +1,13 @@
+import NavbarOne from '../../components/navbar/NavbarOne';
+import FormAddProduct from '../../components/formAddProduct/FormAddProduct';
+import './admin.css'
+
+function Admin (){
+    return (
+        <>
+        <NavbarOne />
+        <FormAddProduct />
+        </>
+    )
+}
+export default Admin;
