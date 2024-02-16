ff94940c98d4fd24adfd793ef6faef8128052705
Yamila <yamilarobles10@gmail.com>
2024-02-16 14:55:49 +0100

    Add initial navbar

--- MarketPlace/src/main.jsx
+++ MarketPlace/src/main.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import User from './views/user/User';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<User />} />
-        <Route path="/admin" element={<Admin />} />
+        {/* <Route path="/admin" element={<Admin />} /> */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
---
+++ MarketPlace/src/views/user/User.jsx
@@ -0,0 +1,10 @@
+import NavbarOne from '../../components/navbar/NavbarOne';
+import './user.css'
+
+
+function User (){
+    return (
+        <NavbarOne />
+    )
+}
+export default User;
