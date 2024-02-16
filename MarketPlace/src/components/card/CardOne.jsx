6dfd317677d9d36228d373c43a9adbccb479d3fc
Maria <ary023@gmail.com>
2024-02-16 20:30:43 +0100

    card products

---
+++ MarketPlace/src/components/card/CardOne.jsx
@@ -0,0 +1,68 @@
+import './cardOne.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { useState, useEffect } from 'react'; // Importa useEffect para llamar a getData una vez que el componente se ha montado
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { productService } from '../../service/productService';
+
+
+
+function CardOne() {
+
+    const [productList, setProductList] = useState([]);
+
+    //esta es la función que carga los datos almacenados en el json
+    async function getData() {
+        try {
+            // Accedo a productService, en concreto a su método GET. 
+            const products = await productService.getAllProducts();
+            // Ahora actualizo el estado de userList con esta variable (usuarios)
+            setProductList(products);
+        } catch (error) {
+            console.error('Error al obtener los datos:', error);
+        }
+    };
+
+    // Llamo a getData() para traer los datos una vez que el componente se ha montado
+    useEffect(() => {
+        getData();
+    }, []); // Pasa un arreglo vacío como segundo argumento para asegurar que getData solo se ejecute una vez
+
+
+
+
+    return (
+        <>
+            <Container>
+                <Row>
+                    {productList.map((product, index) => (
+                        <Col key={index} md={3} className="mb-2">
+                            <Card className="sombreado" style={{ width: '14rem' }}>
+                                <Card.Text style={{ color: 'green', textAlign: 'center', backgroundColor: 'lightgray' }}>
+                                    {product.category}
+                                </Card.Text>
+                                <Card.Img variant="top" src={product.image} />
+                                <Card.Body>
+                                    <Card.Title style={{ color: 'green' }}>{product.name}</Card.Title>
+                                    <Card.Text>
+                                        {product.description}<br></br>
+                                        <span className="precioDestacado">{product.price}</span>
+                                    </Card.Text>
+                                    <div style={{ textAlign: 'center'}}>
+                                        <Button variant="primary">Comprar</Button>
+                                        <span style={{ margin: '0 10px' }}></span> {/* Espacio adicional */}
+                                        <Button variant="primary" disabled>Editar</Button>
+                                    </div>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+        </>
+    );
+}
+export default CardOne;
--- MarketPlace/src/main.jsx
+++ MarketPlace/src/main.jsx
@@ -5,13 +5,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import User from './views/user/User';
+import CardOne from './components/card/CardOne';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<User />} />
+        {/* <Route path="/" element={<User />} /> */}
         {/* <Route path="/admin" element={<Admin />} /> */}
+
+
+        <Route path="/" element={<CardOne/>} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
