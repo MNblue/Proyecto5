45ff24db7b5ce7f3f16df3954f2d028891e83df1
Maria N <ary023@gmail.com>
2024-02-19 19:22:27 +0100

    update edit card

--- MarketPlace/src/components/card/CardDetail.jsx
+++ MarketPlace/src/components/card/CardDetail.jsx
@@ -6,65 +6,76 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { useLocation } from 'react-router-dom';
+import { productService } from '../../service/productService';
 
 
 
 function CardDetail() {
-    const location = useLocation();
-    const product = location.state?.findedProduct;
+  const location = useLocation();
+  const product = location.state?.findedProduct;
 
 
-    const [editable, setEditable] = useState(false);
-    const [editedProduct, setEditedProduct] = useState({ ...product });
+  const [editable, setEditable] = useState(false);
+  const [editedProduct, setEditedProduct] = useState({ ...product });
 
-    //si hacemos clic en el boton de editar entonces la variable editable es true para que se pueda editar ya que al inicio es false
-    const handleEditClick = () => {
-        setEditable(true);
-    };
+  //si hacemos clic en el boton de editar entonces la variable editable es true para que se pueda editar ya que al inicio es false
+  const handleEditClick = () => {
+    setEditable(true);
+  };
 
-    //cuando hagamos cambios en cualquier campo, que esto se indica por name, cambiamos su value asi tomamos el nuevo valor
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setEditedProduct({ ...editedProduct, [name]: value });
-    };
+  //cuando hagamos cambios en cualquier campo, que esto se indica por name, cambiamos su value asi tomamos el nuevo valor
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setEditedProduct({ ...editedProduct, [name]: value });
+  };
 
-    //cuando hacemos clic en el boton guardar, actualizamos los datos en el json y luego la vble editable a false para que se pongan los campos como No editables otra vez
-    const handleSaveClick = () => {
-        //aqui me falta poner el codigo para actualizar en el json..................OJO !!!!!!! cuidadoooo !!!!
+  //cuando hacemos clic en el boton guardar, actualizamos los datos en el json y luego la vble editable a false para que se pongan los campos como No editables otra vez
+  const handleSaveClick = () => {
+    updateData();
+    setEditable(false);
+  };
 
-        setEditable(false);
-    };
+  async function updateData() {
+    try {
+      const products = await productService.updateProduct(editedProduct, editedProduct.id);
+    } catch (error) {
+      console.error('Error al actualizar los datos:', error);
+    }
+  };
 
-
-
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <Card className="sombreado" style={{ width: '44rem' }}>
-                <div style={{ color: 'green', textAlign: 'center', backgroundColor: 'lightgray' }}>
-                    <input type="text" name="category" value={editable ? editedProduct.category : product.category} onChange={handleInputChange} disabled={!editable}  style={{ border: 'none', outline: 'none' }}/>
-                </div>
-                <Card.Img variant="top" src={editedProduct.image || product.image} />
-                <Card.Body>
-                    <Card.Title style={{ color: 'green' }}>
-                        <input type="text" name="name" value={editedProduct.name} onChange={handleInputChange} disabled={!editable}  style={{ border: 'none', outline: 'none' }} />
-                    </Card.Title>
-                    <Card.Text>
-                        <textarea name="description" value={editedProduct.description} onChange={handleInputChange} disabled={!editable}  style={{ border: 'none', outline: 'none' , width: '100%',  overflowX: 'auto' }}/>
-                        <br />
-                        <span className="precioDestacado">
-                            <input type="number" name="price" value={editedProduct.price} onChange={handleInputChange} disabled={!editable}  style={{ border: 'none', outline: 'none'}}/>
-                        </span>
-                    </Card.Text>
-                    <div style={{ textAlign: 'center' }}>
-                        {editable
-                            ? <Button variant="success" onClick={handleSaveClick}>Guardar</Button>
-                            : <Button variant="primary" onClick={handleEditClick}>Editar</Button>
-                        }
-                    </div>
-                </Card.Body>
-            </Card>
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <Card className="sombreado" style={{ width: '44rem' }}>
+        <div style={{ color: 'green', textAlign: 'center', backgroundColor: 'lightgray' }}>
+          {/* <input type="text" name="category" value={editable ? editedProduct.category : product.category} onChange={handleInputChange} disabled={!editable}  style={{ border: 'none', outline: 'none' }}/> */}
+          <select name="category" value={editable ? editedProduct.category : product.category} onChange={handleInputChange} disabled={!editable} style={{ border: 'none', outline: 'none' }}>
+            <option value="huerta">De la huerta a la mesa</option>
+            <option value="artesanias">Artesanías Locales</option>
+            <option value="elaborados">Productos Elaborados</option>
+          </select>
         </div>
-    );
+        <Card.Img variant="top" src={editedProduct.image || product.image} />
+        <Card.Body>
+          <Card.Title style={{ color: 'green' }}>
+            <input type="text" name="name" value={editedProduct.name} onChange={handleInputChange} disabled={!editable} style={{ border: 'none', outline: 'none' }} />
+          </Card.Title>
+          <Card.Text>
+            <textarea name="description" value={editedProduct.description} onChange={handleInputChange} disabled={!editable} style={{ border: 'none', outline: 'none', width: '100%', overflowX: 'auto' }} />
+            <br />
+            <span className="precioDestacado">
+              <input type="number" name="price" value={editedProduct.price} onChange={handleInputChange} disabled={!editable} style={{ border: 'none', outline: 'none' }} />
+            </span>
+          </Card.Text>
+          <div style={{ textAlign: 'center' }}>
+            {editable
+              ? <Button variant="success" onClick={handleSaveClick}>Guardar</Button>
+              : <Button variant="primary" onClick={handleEditClick}>Editar</Button>
+            }
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 export default CardDetail;
