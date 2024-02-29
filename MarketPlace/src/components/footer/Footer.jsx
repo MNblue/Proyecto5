958843529180cc658801fb53d98837a494e23c55
Lorena-Rodriguez <lorenarodriguezc@gmail.com>
2024-02-29 21:12:01 +0100

    add footer

--- MarketPlace/src/components/footer/Footer.jsx
+++ MarketPlace/src/components/footer/Footer.jsx
@@ -1,28 +1,23 @@
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Col, Stack } from 'react-bootstrap';
 import './footer.css'
-
+import homeIcon from '../../img/logoHome.svg';
+import facebook from '../../img/facebook.svg';
+import instagram from '../../img/instagram.svg';
+import twitter from '../../img/twitter.svg';
 
 
 function Footer() {
-    
-    return (
-<>
-<footer>
-    <p>Holaaaaa</p>
-    {/* <Container>
-        <Row>
-            <Col>Logo</Col>
-        </Row>
-        <Row>
-            <Col>RRSS</Col>
-        </Row>
-        <Row>
-            <Col>Text</Col>
-        </Row>
-    </Container> */}
-</footer>
 
-</>
+    return (
+        <>
+            <footer>
+                <Stack direction="horizontal" gap={5} className='text-footer'>
+                    <div className="p-2"><img src={homeIcon} className='logoFooter'/></div>
+                    <div className="p-2 rrssFooter"><img src={facebook} className='fbFooter' /> <img src={instagram}/> <img src={twitter} className='twitterFooter' /></div>
+                    <div className="p-2 ">© InnoConsulting Solutions</div>
+                </Stack>
+            </footer>
+        </>
     )
 }
 
