6eb36ae374ece3afd6590e50966c24448b5e5ea1
hacheproud <helenamillacabrera@gmail.com>
2024-03-04 12:53:21 +0100

    fix test login

--- MarketPlace/src/components/loginPopUp/LoginPopUp.test.js
+++ MarketPlace/src/components/loginPopUp/LoginPopUp.test.js
@@ -1,45 +1,9 @@
-// import React from 'react';
-// import { render, screen, fireEvent } from "@testing-library/react";
-// import '@testing-library/jest-dom';
-// // import { Router } from 'react-router-dom';
-// // import { createMemoryHistory } from 'history';
-// import LoginPopUp from '../loginPopUp/LoginPopUp.jsx';
-
-// describe("<LoginPopUp/>", () => {
-//     test("renders login button when not logged in", () => {
-//         // Crear un history mock
-//         const history = createMemoryHistory();
-
-//         // Renderizar LoginPopUp dentro de un Router
-//         render(
-//             <Router history={history}>
-//                 <LoginPopUp />
-//             </Router>
-//         );
-
-//         const inputContraseña = screen.getByPlaceholderText(/Contraseña/i);
-
-//         expect(inputContraseña).toBeInTheDocument();
-//     });
-// });
-
-// describe("<LoginPopUp/>", () => {
-//     test("renders login button when not logged in", () => {
-//         render(<LoginPopUp/>);
-//         const inputContraseña= screen.getByPlaceholderText(/Contraseña/i);
-
-//         expect(inputContraseña).toBeInTheDocument();
-
-
-//     });
-// })
-
-
 import React from 'react';
 import { render, screen, fireEvent } from "@testing-library/react";
 import '@testing-library/jest-dom';
 import LoginPopUp from '../loginPopUp/LoginPopUp.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const mockedUseNavigate = jest.fn();
 jest.mock('react-router-dom', () => ({
@@ -52,9 +16,11 @@ describe("<LoginPopUp/>",()=>{
 
     test("it should navigate to index", ()=>{
         render(
+          <GoogleOAuthProvider clientId="YOUR_CLIENT_ID">
             <BrowserRouter>
               <LoginPopUp/>
             </BrowserRouter>
+            </GoogleOAuthProvider>
           );
 
           const inputContraseña= screen.getByPlaceholderText(/Contraseña/i);
