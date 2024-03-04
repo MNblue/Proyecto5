7894bef13e9c9465a4ea93a980aa02cd73786968
Maria <ary023@gmail.com>
2024-03-05 00:59:05 +0100

    detail test

--- MarketPlace/src/components/card/CardDetail.jsx
+++ MarketPlace/src/components/card/CardDetail.jsx
@@ -1,4 +1,3 @@
-import './cardDetail.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { useState, useEffect } from 'react'; // Importa useEffect para llamar a getData una vez que el componente se ha montado
@@ -10,11 +9,10 @@ import { useLocation } from 'react-router-dom';
 import { productService } from '../../service/productService';
 import * as formik from "formik";
 import * as yup from "yup";
-import NavbarOne from '../navbar/NavbarOne';
-import LoginPopUp from '../loginPopUp/LoginPopUp';
-import turnleft from './turnleft.png';
-import Footer from '../footer/Footer';
-import { Link } from 'react-router-dom';
+import NavbarOne from "../../components/navbar/NavbarOne";
+import Footer from "../../components/footer/Footer.jsx";
+import LoginPopUp from "../../components/loginPopUp/LoginPopUp";
+import './cardDetail.css';
 
 
 function CardDetail() {
@@ -23,16 +21,22 @@ function CardDetail() {
   const { Formik } = formik;
   const isLogged = location.state?.isLogged;
   const [imageUrl, setImageUrl] = useState(product.file);
-
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const [editable, setEditable] = useState(false);
   const [editedProduct, setEditedProduct] = useState({ ...product });
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
   //si hacemos clic en el boton de editar entonces la variable editable es true para que se pueda editar ya que al inicio es false
   const handleEditClick = () => {
     setEditable(true);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
   //cuando hagamos cambios en cualquier campo, que esto se indica por name, cambiamos su value asi tomamos el nuevo valor
@@ -104,153 +108,67 @@ function CardDetail() {
   // }
 
   return (
-    <>
-      <NavbarOne />
-      <main>
+    <main className='contenedorPpal'>
+      <div className='item1'>
+        <NavbarOne openModal={openModal} isLogged={isLogged} />
+        {isModalOpen && <LoginPopUp closeModal={closeModal} />}
+      </div>
+      <div className='item2'>
+        <Card className="cardStyle" style={{ width: '55rem', display: 'flex', flexDirection: 'row' }}>
 
-        <Link
-          to="/" // Esta línea establece la URL a la que se dirigirá el enlace
-          style={{
-            position: 'absolute',
-            top: '100px',
-            right: '200px',
-            zIndex: '9999',
-            marginTop: '30px',
-            marginRight: '300px',
-            padding: '5px',
-            backgroundColor: 'white',
-            boxShadow: '2px 2px 5px 1px rgba(0, 0, 0, 0.2)',
-            color: 'black',
-            border: 'none',
-            borderRadius: '8px',
-            textDecoration: 'none', // Elimina la subrayado predeterminado del enlace
-            fontFamily: 'Fredoka One',
-
-          }}
-        >
-          Volver <img src={turnleft} style={{ width: '30px', height: '30px' }} />
-        </Link>
-
-        {/* <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <Card className="sombreado" style={{ width: '44rem' }}>
-        <div style={{ color: 'green', textAlign: 'center', backgroundColor: 'lightcolor' }}>
-         
-          <select name="category" value={editable ? editedProduct.category : product.category} onChange={handleInputChange} disabled={!editable} style={{ border: 'none', outline: 'none' }}>
-            <option value="De la huerta a la mesa">De la huerta a la mesa</option>
-            <option value="Artesanía local">Artesanías Locales</option>
-            <option value="Elaborados">Productos Elaborados</option>
-          </select>
-        </div>
-        <Card.Img variant="top" src={imageUrl} />
-
-        {editable && <Button  variant="success" onClick={handleUploadClick}>Cambiar imagen</Button>}
-       
-        <Card.Body>
-          <Card.Title style={{ color: 'green' }}>
-            <input type="text" name="name" value={editedProduct.name} onChange={handleInputChange} disabled={!editable} style={{ border: 'none', outline: 'none' }} />
-          </Card.Title>
-          <Card.Text>
-            <textarea name="description" value={editedProduct.description} onChange={handleInputChange} disabled={!editable} style={{ border: 'none', outline: 'none', width: '100%', overflowX: 'auto' }} />
-            <br />
-            <span className="precioDestacado">
-              <input type="number" name="price" value={editedProduct.price} onChange={handleInputChange} disabled={!editable} style={{ border: 'none', outline: 'none' }} /> €
-            </span><br></br> Stock 
-            <span className="stock">
-              <input type="number" name="stock" value={editedProduct.stock} onChange={handleInputChange} disabled={!editable} style={{ border: 'none', outline: 'none' }} />
-            </span>
-          </Card.Text>
-          <div style={{ textAlign: 'center' }}>
-            {editable 
-              ? <Button variant="success" onClick={handleSaveClick} >Guardar</Button>
-              : <Button variant="primary" onClick={handleEditClick} style={{ display: isLogged? 'inline' : 'none' }}>Editar</Button>
-            }
+    <div style={{ position: 'relative', display: 'inline-block' }}>
+          <Card.Img variant="top" src={imageUrl} style={{ width: '400px', height: '400px', marginLeft: '10px', marginBottom: '10px', marginTop: '10px', borderRadius: '8px', borderColor:'#aca6a6',border:'1px solid' }} />
+          {editable && <button className='btn3' onClick={handleUploadClick} style={{ position: 'absolute', bottom: '18px', right: '10px'}}>Cambiar imagen</button>}
           </div>
-        </Card.Body>
-      </Card>
-       
-    </div> */}
 
+          <Card.Body style={{ backgroundColor: '#F5F5F5', marginRight: '10px', marginLeft: '10px', marginBottom: '10px', marginTop: '10px', borderRadius: '8px' }}>
+            <div style={{ display: 'flex' }}>
+              <div style={{ marginBottom: '20px' }}>
+                <label htmlFor='name'>Producto:</label>
+                <input type="text" id='name' name="name" value={editedProduct.name} onChange={handleInputChange} disabled={!editable} style={{ border: 'none', outline: 'none', backgroundColor: '#EBE8E8', borderRadius: '8px', boxShadow: '2px 2px 5px 1px rgba(0, 0, 0, 0.2)', width: '200px' }} />
+              </div>
+              <div >
+                Categoría:
+                <select name="category" value={editable ? editedProduct.category : product.category} onChange={handleInputChange} disabled={!editable} style={{ border: 'none', outline: 'none', backgroundColor: '#EBE8E8', borderRadius: '8px', boxShadow: '2px 2px 5px 1px rgba(0, 0, 0, 0.2)' }}>
+                  <option value="huerta">De la huerta a la mesa</option>
+                  <option value="artesanias">Artesanías Locales</option>
+                  <option value="elaborados">Productos Elaborados</option>
+                </select>
+              </div>
+            </div>
 
+            Descripción:
+            <textarea name="description" value={editedProduct.description} onChange={handleInputChange} disabled={!editable} style={{ border: 'none', outline: 'none', width: '100%', height: '150px', overflowX: 'auto', backgroundColor: '#EBE8E8', borderRadius: '8px', boxShadow: '2px 2px 5px 1px rgba(0, 0, 0, 0.2)' }} />
 
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+              <div >
+                Precio:
+                <input type="number" name="price" value={editedProduct.price} onChange={handleInputChange} disabled={!editable} style={{ width: '200px', border: 'none', outline: 'none', backgroundColor: '#EBE8E8', borderRadius: '8px', boxShadow: '2px 2px 5px 1px rgba(0, 0, 0, 0.2)' }} />
+              </div>
+              <div>
+                Stock:
+                <input type="number" name="stock" value={editedProduct.stock} onChange={handleInputChange} disabled={!editable} style={{ width: '200px', border: 'none', outline: 'none', backgroundColor: '#EBE8E8', borderRadius: '8px', boxShadow: '2px 2px 5px 1px rgba(0, 0, 0, 0.2)' }} />
+              </div>
+            </div>
 
+            <div style={{ textAlign: 'center', marginTop: '20px' }}>
+              {editable ? (
+                <>
+                  <Button className='btn1' onClick={handleSaveClick}>Guardar</Button>
+                  
+                </>
+              ) : (
+                <Button className='btn2' onClick={handleEditClick} style={{ display: isLogged ? 'inline' : 'none' }}>Editar</Button>
+              )}
+            </div>
 
-
-
-
-        <Container className="containerDetail" style={{ width: '30%' }}>
-          <Row >
-            {/* <Col xs="auto" className="d-flex align-items-center justify-content-center" style={{backgroundColor:'green'}}> */}
-
-            <Col>
-              <Row>
-
-
-                <Col md={3} className="detail">
-                  <Card className="classCategory1" style={{ width: '40rem', height: '50rem', display: 'flex', flexDirection: 'column', boxShadow: '2px 2px 5px 1px rgba(0, 0, 0, 0.2)' }}>
-
-                    <Card.Text style={{ textAlign: 'center', marginTop: '20px', paddingBottom: '0px', marginBottom: '0px', fontSize: '20px', color: 'black',}}>
-
-                      {/* <div className={isLogged ? 'btnDeleteDch' : 'btnDeleteDchUser'}> */}
-                      {/* <div > {product.category}</div> */}
-                      {/* <div style={{ textAlign:'right'}}>
-                                                    {isLogged && (<button disabled={!isLogged} onClick={() => handleClickDelete(product.id)} style={{ border: 'none', background: 'none', padding: 0, cursor: 'pointer', outline: 'none', marginLeft: 'auto' }}><img src='/src/components/card/delete.png' style={{ width: '14px', height: '14px', border: 'none' }} /></button>)}
-                                                </div>
-                                            </div> */}
-
-                    </Card.Text>
-                    <div style={{ display: 'flex', flexDirection: 'column', paddingTop: '0px', gap: '20px', textAlign: 'center', position: 'relative' }}>
-                      <Card.Img variant="top" src={product.file} className='imgCardDetail' />
-
-                    </div>
-
-                    <Card.Body style={{ flex: '1', padding: '0px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                      <div style={{ display: 'flex', flexDirection: 'column', gap: '15px', alignItems: 'center' }}>
-                        <div style={{ display: 'flex', flexDirection: 'row', gap: '15px', alignItems: 'center' }}>
-                          <div>
-                            <Card.Title className='textCategoryName'>Categoría: {product.category}</Card.Title>
-                          </div>
-
-                          <div>
-                            <Card.Title className='textProductName'>Producto: {product.name}</Card.Title>
-                          </div>
-
-                        </div>
-                        
-                        <Card.Text className="description" style={{ textAlign: 'center', margin: '0 20px' }}> Descripción: 
-                          {product.description}<br></br>
-                        </Card.Text>
-
-                        <div style={{ display: 'flex', flexDirection: 'row', gap: '15px', alignItems: 'center' }}>
-                          <div className='boxSmallDetail' style={{ textAlign: 'center' }}>
-                            Unidades: {product.stock}
-                          </div>
-
-                          <div className='boxSmallPriceDetail' style={{ textAlign: 'center' }}>
-                            Precio: {product.price} €
-                          </div>
-                          
-                        </div>
-                      </div>
-                    </Card.Body>
-
-
-                  </Card>
-                </Col>
-
-              </Row>
-            </Col>
-
-
-
-          </Row>
-        </Container>
-
-
-
-
-      </main>
-      <Footer />
-    </>
+          </Card.Body>
+        </Card>
+      </div>
+      <div className='item3'>
+        <Footer />
+      </div>
+    </main>
   );
 }
 
